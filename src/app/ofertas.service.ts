ee390d92b501d05738ba7b926d3357b001c072a6
fabioguelfi <fabioguelfiunix@gmail.com>
2018-03-10 18:06:02 -0300

    Exibindo ofertas com base no serviço OfertasService parte 2

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -6,7 +6,7 @@ export class OfertasServices {
     {
       id: 1,
       categoria: "restaurante",
-      titulo: "Super Burger",
+      titulo: "Super Burger Guelfi",
       descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
       anunciante: "Original Burger",
       valor: 29.90,

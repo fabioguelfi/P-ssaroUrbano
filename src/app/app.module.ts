568b9ce9472f47cf9fa03276e8a8714c50054e7e
fabioguelfi <fabioguelfiunix@gmail.com>
2018-03-11 18:52:32 -0300

    Criando as páginas Restaurantes e Diversão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import { RodapeComponent } from './rodape/rodape.component';
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
     HttpModule,

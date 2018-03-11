a028e1fa6c72e05e710d88c6320cd0f880265c8b
fabioguelfi <fabioguelfiunix@gmail.com>
2018-03-11 19:51:57 -0300

    Recuperando ofertas do tipo Diversao (HTTP Resquest / API Rest / Promise)

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasServices } from '../ofertas.service';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasServices]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public diversoes: Oferta[]
+
+  constructor(private ofertaService: OfertasServices) { }
 
   ngOnInit() {
+    this.ofertaService.getOfertasPorCategoria('diversao')
+    .then((diversoes: any) => {
+      this.diversoes = diversoes
+    })
   }
 
 }

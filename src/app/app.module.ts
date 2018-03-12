a3711d813b976c354718ad4a48cadd753ef0bd46
fabioguelfi <fabioguelfiunix@gmail.com>
2018-03-11 22:39:28 -0300

    Criando os componentes ComoUsarComponent e OndeFicaComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 
 @NgModule({
@@ -23,7 +25,9 @@ import { OfertaComponent } from './oferta/oferta.component';
     RodapeComponent,
     RestaurantesComponent,
     DiversaoComponent,
-    OfertaComponent
+    OfertaComponent,
+    ComoUsarComponent,
+    OndeFicaComponent
   ],
   imports: [
     HttpModule,

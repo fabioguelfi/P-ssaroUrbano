c03ba0fcebf8ca1a49df781d6bb1b8d85755517a
fabioguelfi <fabioguelfiunix@gmail.com>
2018-03-12 23:47:30 -0300

    Observables - Interval operator

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasServices } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-oferta',
@@ -24,11 +26,17 @@ export class OfertaComponent implements OnInit {
         this.oferta = oferta
       })
 
-      this.route.params.subscribe(
-        (parametro: any) =>  { console.log(parametro) },
-        (erro: any) => { console.log(erro) },
-        () => console.log('processamento classificado como conluido')
-      )
+      let tempo = Observable.interval(3000)
+
+      tempo.subscribe((intervalo: number) => {
+        console.log(intervalo)
+      })
+
+      // this.route.params.subscribe(
+      //   (parametro: any) =>  { console.log(parametro) },
+      //   (erro: any) => { console.log(erro) },
+      //   () => console.log('processamento classificado como conluido')
+      // )
 
     }
 

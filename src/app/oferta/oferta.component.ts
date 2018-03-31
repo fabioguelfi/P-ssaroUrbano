a9fa2aec282e287b3ffef59bd6247ca2e463936b
fabio guelfi <fabioguelfiunix@gmail.com>
2018-03-31 14:36:31 -0300

    Observables - Memory leak e Unsubscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasServices } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-oferta',
@@ -12,7 +13,10 @@ import 'rxjs/Rx';
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasServices]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  private tempoObservableSubscripition: Subscription
+  private meuObservableSubscripition: Subscription
 
   public oferta: Oferta
 
@@ -23,41 +27,46 @@ export class OfertaComponent implements OnInit {
   ngOnInit() {
 
     this.ofertasServices.getOfertaPorId(this.route.snapshot.params['id'])
-      .then(( oferta: Oferta ) => {
+      .then((oferta: Oferta) => {
         this.oferta = oferta
       })
 
-      //observable (observavel)
-      let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-        observer.next(1)
-        observer.next(2)
-        observer.next(3)
-        observer.next(4)
-        observer.next(5)
-        observer.next(6)
-        observer.next(7)
-        observer.complete()
-      })
+    //observable (observavel)
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(1)
+      observer.next(2)
+      observer.next(3)
+      observer.next(4)
+      observer.next(5)
+      observer.next(6)
+      observer.next(7)
+      observer.complete()
+    })
 
-      //observable (observador)
-      meuObservableTeste.subscribe(
-        (resultado: number) => console.log(resultado + 10),
-        (erro: String) => console.log(erro),
-        () => console.log('a stream de eventos foi finalizada')
-      )
+    let tempo = Observable.interval(3000)
 
-      // let tempo = Observable.interval(3000)
+    this.tempoObservableSubscripition = tempo.subscribe((intervalo: number) => {
+      console.log(intervalo)
+    })
 
-      // tempo.subscribe((intervalo: number) => {
-      //   console.log(intervalo)
-      // })
+    //observable (observador)
+    this.meuObservableSubscripition = meuObservableTeste.subscribe(
+      (resultado: number) => console.log(resultado + 10),
+      (erro: String) => console.log(erro),
+      () => console.log('a stream de eventos foi finalizada')
+    )
 
-      // this.route.params.subscribe(
-      //   (parametro: any) =>  { console.log(parametro) },
-      //   (erro: any) => { console.log(erro) },
-      //   () => console.log('processamento classificado como conluido')
-      // )
+    // this.route.params.subscribe(
+    //   (parametro: any) =>  { console.log(parametro) },
+    //   (erro: any) => { console.log(erro) },
+    //   () => console.log('processamento classificado como conluido')
+    // )
 
-    }
+  }
+
+  ngOnDestroy() {
+    this.meuObservableSubscripition.unsubscribe()
+    this.tempoObservableSubscripition.unsubscribe()
+  }
 
 }

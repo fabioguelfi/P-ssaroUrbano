0f896cc65b5527c4c9f77577fa1548bcfb5aba05
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-01 10:55:01 -0300

    Lógica da pesquisa de ofertas - DistinctUntilChanged

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs/Subject';
 
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/observable/of';
 
 @Component({
@@ -24,9 +25,10 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
     this.ofertas = this.subjectPesquisa // retorno Oferta[]
       .debounceTime(1000)
+      .distinctUntilChanged()
       .switchMap((termo: string) => {
         console.log('requisicao http para api', termo)
-        if(termo.trim() === '') {
+        if (termo.trim() === '') {
           return Observable.of(<Oferta[]>([]))
         }
         return this.ofertasServices.pesquisaOfertas(termo)

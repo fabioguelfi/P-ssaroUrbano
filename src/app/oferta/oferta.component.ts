a368a357f1c5e7485c12a2893fce5963fdbfdcc0
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-01 18:17:30 -0300

    Atualizando oferta no processo de mudança de rota

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasServices } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -20,10 +20,14 @@ export class OfertaComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    this.ofertasServices.getOfertaPorId(this.route.snapshot.params['id'])
-      .then((oferta: Oferta) => {
-        this.oferta = oferta
-      })
+    this.route.params.subscribe((parametros: Params) => {
+
+      this.ofertasServices.getOfertaPorId(parametros.id)
+        .then((oferta: Oferta) => {
+          this.oferta = oferta
+        })
+
+    })
 
   }
 

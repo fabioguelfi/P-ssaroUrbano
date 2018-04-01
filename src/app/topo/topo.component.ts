b3e57d265eaf4d266c89ff2193f1df99d012277f
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-01 11:48:50 -0300

    Lógica da pesquisa de ofertas - Exibindo ofertas

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -19,32 +19,52 @@ import 'rxjs/add/observable/of';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public oferta: Oferta[]
   private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasServices: OfertasServices) { }
 
   ngOnInit() {
+
     this.ofertas = this.subjectPesquisa // retorno Oferta[]
-      .debounceTime(1000)
-      .distinctUntilChanged()
+
+      .debounceTime(1000) // executa a acao do switchmap apos 1 segundo
+
+      .distinctUntilChanged() // para fazer pesquisas distintas
+
       .switchMap((termo: string) => {
-        console.log('requisicao http para api', termo)
+
         if (termo.trim() === '') {
+
+          // retorna um observable de ofertas vazio
           return Observable.of(<Oferta[]>([]))
+
         }
+
         return this.ofertasServices.pesquisaOfertas(termo)
+
       })
       .catch((err: any) => {
+
         console.log(err)
         return Observable.of<Oferta[]>([])
+
       })
-    this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas))
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+
+      this.oferta = ofertas
+
+    })
 
   }
 
   public pesquisa(termoDaBusca: string): void {
+
     console.log('keyup caractere', termoDaBusca)
+
     this.subjectPesquisa.next(termoDaBusca)
+
   }
 
 }

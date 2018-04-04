294379bdcca8be091daf0e21107ad87dd7929f30
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-03 21:01:52 -0300

    Criando o model ItemCarrinho

---
+++ src/app/shared/item-carrinho.model.ts
@@ -0,0 +1,11 @@
+class ItemCarrinho {
+    constructor(
+        public id: number,
+        public img: object,
+        public titulo: string,
+        public descricao_oferta: string,
+        public valor: number
+    ) { }
+}
+
+export { ItemCarrinho }

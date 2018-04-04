91101053908fb92ae42266b29c7fde8d1d5088fb
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-03 23:01:19 -0300

    Exibindo itens do carrinho de compras em OrdemCompra

--- src/app/order-compra/order-compra.component.ts
+++ src/app/order-compra/order-compra.component.ts
@@ -3,6 +3,7 @@ import { Pedido } from './../shared/pedido.model';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CarrinhoService } from '../carrinho.service';
+import { ItemCarrinho } from '../shared/item-carrinho.model';
 
 @Component({
   selector: 'app-order-compra',
@@ -14,6 +15,7 @@ import { CarrinhoService } from '../carrinho.service';
 export class OrderCompraComponent implements OnInit {
 
   public idPedidoCompra: number = undefined
+  public itensCarrinho: ItemCarrinho[] = []
 
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
@@ -28,7 +30,7 @@ export class OrderCompraComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log(this.carrinhoService.exibirItens())
+    this.itensCarrinho = this.carrinhoService.exibirItens()
   }
 
   public confirmarCompra(): void {
